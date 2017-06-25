df6a540ec09ec3f1848d3fc06c98d05ebb88a51c
Sergey Lazarev <R-e-v-a-n@users.noreply.github.com>
2017-06-25 16:00:43 +0700

    fix

--- target/classes/database.sql
+++ target/classes/database.sql
@@ -1,34 +1,27 @@
--- Table: users
-CREATE TABLE users (
-  id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-)
-  ENGINE = InnoDB;
-
 -- Table: roles
 CREATE TABLE roles (
-  id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  id   INT          NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+
+  PRIMARY KEY(id)
 )
   ENGINE = InnoDB;
 
--- Table for mapping user and roles: user_roles
-CREATE TABLE user_roles (
-  user_id INT NOT NULL,
+-- Table: users
+CREATE TABLE users (
+  id       INT          NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   role_id INT NOT NULL,
 
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (role_id) REFERENCES roles (id),
-
-  UNIQUE (user_id, role_id)
+  PRIMARY KEY(id),
+  UNIQUE(username),
+  FOREIGN KEY(role_id) REFERENCES roles(id)
 )
   ENGINE = InnoDB;
 
 -- Insert data
-INSERT INTO users VALUES (1, 'admin', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
-
 INSERT INTO roles VALUES (1, 'ROLE_USER');
 INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO user_roles VALUES (1, 2);
+INSERT INTO users VALUES (1, 'admin', '$2a$11$euOWBMHVmiOVtR/nvS4bLuWKOmbGtS8Ac89pw8Wdhs.p5xWnL7j5y', 2);
