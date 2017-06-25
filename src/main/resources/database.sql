176eb80f3618a6ac81186742642b535ddbb46139
Sergey Lazarev <R-e-v-a-n@users.noreply.github.com>
2017-06-25 15:49:10 +0700

    Change database. Add index UNIQUE for username column.

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,17 +1,21 @@
 -- Table: roles
 CREATE TABLE roles (
-  id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  id   INT          NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+
+  PRIMARY KEY(id)
 )
   ENGINE = InnoDB;
 
 -- Table: users
 CREATE TABLE users (
-  id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id       INT          NOT NULL AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   role_id INT NOT NULL,
 
+  PRIMARY KEY(id),
+  UNIQUE(username),
   FOREIGN KEY(role_id) REFERENCES roles(id)
 )
   ENGINE = InnoDB;
@@ -20,4 +24,4 @@ CREATE TABLE users (
 INSERT INTO roles VALUES (1, 'ROLE_USER');
 INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO users VALUES (1, 'admin', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG', 2);
+INSERT INTO users VALUES (1, 'admin', '$2a$11$euOWBMHVmiOVtR/nvS4bLuWKOmbGtS8Ac89pw8Wdhs.p5xWnL7j5y', 2);
